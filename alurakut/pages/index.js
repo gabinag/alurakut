d3104d45cb1716d96b8040756cb94d283c74193a
Gabriela Nagamuta <gnagamuta03@gmail.com>
2021-07-12 12:22:07 -0300

    first commit

---
+++ alurakut/pages/index.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+//const Title = styled.h1`
+//  font-size: 50px;
+//  color: ${({ theme }) => theme.colors.primary};
+//`
+
+const Box = styled.div`
+  background: #fff;
+  border-radius: 8px;
+`;
+
+export default function Home() {
+  return (
+  <main>
+    <Box>
+      Imagem
+    </Box>
+    <Box>
+      Bem vindo
+    </Box>
+    <Box>
+      Comunidades
+    </Box>
+  </main>
+  )
+}
