fb505ad80278b4f8213f13731192dd437092e8b9
Gabriela Nagamuta <gnagamuta03@gmail.com>
2021-07-12 13:06:05 -0300

    Add reset and other styles

--- alurakut/pages/_app.js
+++ alurakut/pages/_app.js
@@ -1,10 +1,26 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+
+  /* Reset CSS*/
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color: #D9E6F6;
+    font-family: sans-serif;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
   }
 `
 
